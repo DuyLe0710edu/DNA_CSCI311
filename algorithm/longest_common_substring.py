e95a7f364f309184f424d6f40566a0e24a2b48fc
Duy <dl039@bucknell.edu>
2025-04-02 16:09:11 -0400

    finale

--- algorithm/longest_common_substring.py
+++ algorithm/longest_common_substring.py
@@ -1,4 +1,4 @@
-def longest_common_substring(query, database):
+def longest_common_substring(query, database): 
     """
     Find the sequence in the database that has the longest common substring with the query.
     
@@ -9,29 +9,45 @@ def longest_common_substring(query, database):
     Returns:
         tuple: A (header, sequence) tuple of the best match
     """
-    best = None
-    max_len = 0
     
-    for head, seq in database:
-        # Find longest common substring between query and this sequence
-    #     m, n = len(query), len(seq)
-    #     dp = [[0] * (n + 1) for _ in range(m + 1)]
-    #     len_so_far = 0
-    #     end_idx = 0
-        
-    #     for i in range(1, m + 1):
-    #         for j in range(1, n + 1):
-    #             if query[i - 1] == seq[j - 1]:
-    #                 dp[i][j] = dp[i - 1][j - 1] + 1
-    #                 if dp[i][j] > len_so_far:
-    #                     len_so_far = dp[i][j]
-    #                     end_idx = i
-        
-    #     if len_so_far > max_len:
-    #         max_len = len_so_far
-    #         best = (head, seq)
-    
-    # return best
-    #you can do your own code in here Molly
+    #Determine longest substring from one sequence 
+    def longestForSequence(s, t):
+        commonSubstrings=[]
 
-     pass
+        for i in range(len(s)):      # Determine start point for comparisons with s
+            for j in range(len(t)):  # Determine start point for comparisons with t
+                sIdx=i
+                tIdx=j
+                str=""
+                done=False
+                while not done:
+                    if (tIdx>=len(t)):                          # Stop search before reaching out of bounds index
+                        done=True
+                    elif s[sIdx:sIdx+1]==t[tIdx:tIdx+1]:        # If characters match, increment both indices
+                        str+=s[sIdx:sIdx+1]
+                        sIdx+=1
+                        tIdx+=1
+                    elif s[sIdx:sIdx+1]!=t[tIdx:tIdx+1]:        # If no match, exit while loop to start search at next index
+                        if len(str)!=0:
+                            commonSubstrings.append(str)
+                        done=True
+                if len(str)!=0:
+                    commonSubstrings.append(str)                # Append any leftover common substring to commonSubstrings (when common substring includes end of t)
+
+        longest=""
+        for item in commonSubstrings:       # Iterate through commonSubstrings
+            if len(item)>len(longest):      # If longer than longest, store as longest
+                longest=item
+
+        return longest 
+    
+    # Determine best sequence from results of longest substring comparison
+    longestOverall=""
+    bestSeq=()
+    for head, seq in database:
+        longestSubsequence=longestForSequence(seq,query)
+        if len(longestSubsequence)>len(longestOverall):
+            longestOverall=longestSubsequence
+            bestSeq=(head,seq)
+
+    return bestSeq
